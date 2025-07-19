8698decb9847726d785ae7b176c798f266709de4
katafuchix <katafuchix@gmail.com>
2025-07-19 21:36:03 +0900

    Initial Commit

---
+++ SwiftUI_CustomHeaderForScrollExample/SwiftUI_CustomHeaderForScrollExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomHeaderForScrollExampleApp.swift
+//  SwiftUI_CustomHeaderForScrollExample
+//
+//  Created by cano on 2025/07/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomHeaderForScrollExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
